7e9e8f1f0a8be88f5cc1adb5dd04779e6ed60f80
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-08-28 11:45:16 -0400

    Footer links

--- src/App.js
+++ src/App.js
@@ -24,6 +24,24 @@ const App = () => {
         onChange={onChangeSearch}
       />
       <CardList monsters={monsters} />
+      <p>
+        Created by{" "}
+        <a
+          href="https://www.linkedin.com/in/alejandrrhernandez/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Alejandro Hernández
+        </a>
+        {" - "}
+        <a
+          href="https://github.com/Alejandrehl/monster-rolodex"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Github Repository
+        </a>
+      </p>
     </div>
   );
 };
