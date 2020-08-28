a87e5101c8c3aa31cb197ce0d47eeefaf878b38b
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-08-28 11:34:43 -0400

    Search input

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { CardList } from "./components/card-list/card-list.component";
+import "./app.styles.css";
 
 const App = () => {
   const [monsters, setMonsters] = useState([]);
+  const [searchField, setSearchField] = useState("");
+
+  const onChangeSearch = (e) => setSearchField(e.target.value);
 
   useEffect(() => {
     fetch("https://jsonplaceholder.typicode.com/users")
@@ -12,7 +16,13 @@ const App = () => {
   }, []);
 
   return (
-    <div>
+    <div className="App">
+      <input
+        type="search"
+        placeholder="Search monster"
+        value={searchField}
+        onChange={onChangeSearch}
+      />
       <CardList monsters={monsters} />
     </div>
   );
--- src/components/card/card.component.js
+++ src/components/card/card.component.js
@@ -9,6 +9,7 @@ export const Card = ({ monster }) => {
         src={`https://robohash.org/${monster.id}?set=set2&size=180x180`}
       />
       <h2>{monster.name}</h2>
+      <p>{monster.email}</p>
     </div>
   );
 };
