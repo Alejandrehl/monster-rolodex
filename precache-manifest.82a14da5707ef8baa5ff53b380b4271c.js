ced25a6feef1831787c73f2b1b3c7f8d4a598028
Alejandro Exequiel Hernández Lara <Alejandrehl@icloud.com>
2020-08-29 16:19:21 -0400

    Updates

---
+++ precache-manifest.82a14da5707ef8baa5ff53b380b4271c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d039ebcf45422a117c25e8f3717fb74b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ac30bad0c3672484a6cc",
+    "url": "/monsters-rolodex/static/css/main.e1d53b6f.chunk.css"
+  },
+  {
+    "revision": "43cd1e4b3cd0a6e923d0",
+    "url": "/monsters-rolodex/static/js/2.51b5c0f4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.51b5c0f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac30bad0c3672484a6cc",
+    "url": "/monsters-rolodex/static/js/main.9a2f79c5.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
